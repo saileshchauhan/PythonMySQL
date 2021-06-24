d2429d149d3cfc13009a3d0001e30dca68519aca
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-24 16:37:34 +0530

    [sailesh] Add: insert record

--- Python_CRUD/CRUD_Methods.py
+++ Python_CRUD/CRUD_Methods.py
@@ -5,7 +5,7 @@
 @Title : This Script provide method for CRUD operation in MySQL Database.
 '''
 
-import logging
+import logging,re
 import LogConfig
 from decouple import config
 import mysql.connector
@@ -32,5 +32,45 @@ class CRUD_Methods:
         except Exception as ex:
             logging.critical(ex)
     
+    def validation(value):
+        '''
+        Description:
+            Method validates values given as parameters.
+        Parameters:
+            Takes value as parameter.
+        Return:
+            value after validating.
+        '''
+        try:
+            regexFloat="^[A-Z]{1,}[a-z]{2,}$"
+            if(re.match(regexFloat,value)):
+                return str(value)
+            logging.info("Invalid FirstName")
+            quit()
+        except Exception as ex:
+            logging.critical(ex)
+    
+    def new_entry(self):
+        try:
+            mycursor=self._db.cursor()
+            name=CRUD_Methods.validation(input("Enter First Name"))
+            address=CRUD_Methods.validation(input("Enter Address"))
+            value=(name,address)
+            query="INSERT INTO student (NAME,ADDRESS) VALUES (%s,%s)"
+            mycursor.execute(query,value)
+            self._db.commit()
+            logging.info(mycursor.rowcount,'record inserted.')
+        except Exception as ex:
+            logging.critical(ex)
+    
+    def read_table(self):
+        try:
+            mycursor=self._db.cursor()
+            mycursor.execute('SELECT * FROM STUDENT')
+            rows=mycursor.fetchall()
+            for row in rows:
+                print(row)
+        except Exception as ex:
+            logging.critical(ex)
 
 
