e702066f6d4e34ff409f0996b4b0ec0a8aa2b2a0
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-24 14:13:12 +0530

    [sailesh] Add:Main file,logger file, .env file

---
+++ Python_CRUD/CRUD_Methods.py
@@ -0,0 +1,15 @@
+'''
+@Author: Sailesh Chauhan
+@Date: 24-06-2021
+@Last Modified by: Sankar
+@Last Modified time: 24-06-2021
+@Title : This Script provide method for CRUD operation in MySQL Database.
+'''
+
+import LogConfig
+
+class CRUD_Methods:
+
+    def __init__(self) -> None:
+        pass
+    pass
---
+++ Python_CRUD/Main.py
@@ -0,0 +1,8 @@
+'''
+@Author: Sailesh Chauhan
+@Date: 24-06-2021
+@Last Modified by: Sankar
+@Last Modified time: 24-06-2021
+@Title : To Perform CRUD operation in MySQL database using Python.
+'''
+
