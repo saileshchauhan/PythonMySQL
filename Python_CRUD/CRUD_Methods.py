a8d150d5b12668be99d9970454a21a50e9fa9cbe
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-24 15:48:14 +0530

    [sailesh] Add: connection method

--- Python_CRUD/CRUD_Methods.py
+++ Python_CRUD/CRUD_Methods.py
@@ -1,15 +1,36 @@
 '''
 @Author: Sailesh Chauhan
 @Date: 24-06-2021
-@Last Modified by: Sankar
 @Last Modified time: 24-06-2021
 @Title : This Script provide method for CRUD operation in MySQL Database.
 '''
 
+import logging
 import LogConfig
+from decouple import config
+import mysql.connector
 
 class CRUD_Methods:
 
-    def __init__(self) -> None:
-        pass
-    pass
+    def __init__(self):
+        self._host=config('host')
+        self._userName=config('user')
+        self._passWord=config('passwd')
+        self._databaseName=config('database')
+    
+    def new_connection(self):
+        try:
+            db=mysql.connector.connect(
+                host=self._host,
+                user=self._userName,
+                passwd=self._passWord,
+                database=self._databaseName
+                )
+            if(db.is_connected):
+                logging.info("Connection Succesful")
+            self._db=db
+        except Exception as ex:
+            logging.critical(ex)
+    
+
+
