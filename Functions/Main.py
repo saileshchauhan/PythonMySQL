c33fee26d945157373e649494db99c99958b7304
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-25 12:14:04 +0530

    [sailesh] Add: fuctions main method

---
+++ Functions/Main.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sailesh Chauhan
+@Date: 24-06-2021
+@Last Modified time: 24-06-2021
+@Title : To Perform CRUD operation in MySQL database using Python.
+'''
+
+from MySQL_Functions import Functions
+
+
+class Main:
+    pass
---
+++ Functions/MySQL_Functions.py
@@ -0,0 +1,26 @@
+'''
+@Author: Sailesh Chauhan
+@Date: 24-06-2021
+@Last Modified time: 24-06-2021
+@Title : This Script provide method for CRUD operation in MySQL Database.
+'''
+import sys
+
+from mysql.connector import cursor
+
+sys.path.append(r"D:\PythonMySQL\Functions")
+import LogConfig
+import logging,re
+
+from decouple import config
+import mysql.connector
+
+class Functions:
+
+    def __init__(self):
+        self.host=config('host')
+        self.userName=config('user')
+        self.passWord=config('passwd')
+        self.databaseName=config('database')
+        self.db=self.new_connection()
+
