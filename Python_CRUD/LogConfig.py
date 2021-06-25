65007f0c8ece4a9bdd0dd08586de315fcfd003ac
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-25 15:42:34 +0530

    [sailesh] Refactor: Python CRUD

---
+++ Python_CRUD/LogConfig.py
@@ -0,0 +1,5 @@
+import logging
+from decouple import config
+
+FILE_NAME=config('logFilePath')
+logging.basicConfig(filename=FILE_NAME,level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
