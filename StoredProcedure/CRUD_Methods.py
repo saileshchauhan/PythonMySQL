d9a8c0f910a634420330b88a4ed09f9a539b1329
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-27 22:31:27 +0530

    [sailesh] Add: methods and main class

---
+++ StoredProcedure/CRUD_Methods.py
@@ -0,0 +1,20 @@
+'''
+@Author: Sailesh Chauhan
+@Date: 27-06-2021
+@Last Modified time: 27-06-2021
+@Title : This Script provide method for CRUD operation in MySQL Database using
+         procedure.
+'''
+import sys
+
+from mysql.connector.errors import get_mysql_exception
+
+sys.path.append(r"D:\PythonMySQL\StoredProcedure")
+import LogConfig
+import logging,re
+
+from decouple import config
+import mysql.connector
+
+class CRUD_Methods:
+    pass
---
+++ StoredProcedure/Main.py
@@ -0,0 +1,17 @@
+'''
+@Author: Sailesh Chauhan
+@Date: 27-06-2021
+@Last Modified time: 27-06-2021
+@Title : This Script provide method for CRUD operation in MySQL Database using
+         procedure.
+'''
+
+import logging
+from random import choice
+from CRUD_Methods import CRUD_Methods
+
+
+class Main:
+
+    
+    pass
