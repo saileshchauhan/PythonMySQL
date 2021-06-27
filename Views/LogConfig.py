ae63415b6b07ad44a7abb78c4b49cec0af993298
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-27 22:54:48 +0530

    [sailesh] Refactor:Branching

--- Views/LogConfig.py
+++
@@ -1,6 +0,0 @@
-import logging
-from decouple import config
-
-FILE_NAME=config('logFilePath')
-logging.basicConfig(filename=FILE_NAME,level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
-
